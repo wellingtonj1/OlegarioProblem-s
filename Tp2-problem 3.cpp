cf28b921a777d1c60aad1f681b1026693b093d44
JapaBits <34574051+wellingtonj1@users.noreply.github.com>
2018-08-13 07:59:26 -0300

    Create Tp2-problem 3.cpp

---
+++ Tp2-problem 3.cpp
@@ -0,0 +1,65 @@
+/*Problema Calculadora de Números Complexos
+1.
+Desenvolva,  usando  a  UML  (  diagrama  de  classe  ),  uma  classe  que 
+modele   um   objeto 
+número   complexo
+   em   conformidade   com   o 
+paradigma orientado a objeto. Posteriormente implemente esta classe. 
+O   objeto   número   complexo   tem   as   seguintes   funcionalidades: 
+inicialização  do  número,  adição,  subtração,  multiplicação,  divisão, 
+igualdade e diferença. Considerações: 
+a)
+um  número  complexo  possui  a  seguinte  forma:  parte  real  +  parte 
+imaginária * i (a+bi) onde i é a raiz quadrada de -1;
+b)
+inicialização de um  número
+ complexo;
+c)
+obter   um   número   complexo,   devolver   um
+ número complexo 
+encapsulado usando a notação a + bi;
+d)
+verificar a igualdade
+ entre dois números complexos;
+e)
+verificar a diferença entre dois números complexos;
+f)
+adicionar dois números complexos - fórmula: 
+✴
+(a+bi)+(c+di) = (a+c)+(b+d)i;
+g)
+subtrair dois números complexos - fórmula
+:
+✴
+(a+bi)
+−
+(c+di) = (a
+−
+c)+(b
+−
+d)i;
+h)
+multiplicar dois números complexos - fórmula
+:
+✴
+(a+bi)
+∗
+(c+di) = (ac
+−
+bd)+(ad+bc)i;
+i)
+dividir dois números complexos - fórmula: 
+✴
+(a+bi)/(c+di) = (ac+bd)/(c
+2
++d
+2
+) + ((bc-ad)/(c
+2
++d
+2
+))i.
+2.
+Construir  uma  calculador  de  números  complexos  utilizando  a  classe 
+descrita  no  item  1.  Utilizar  o  QT  para  construir  a  interface  gráfica  da 
+calculadora.*/
